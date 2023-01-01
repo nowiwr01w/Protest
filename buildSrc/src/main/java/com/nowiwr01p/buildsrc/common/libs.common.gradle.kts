f9ba4b21617d50542067beb29cf865af87eb35f2
Andrey Larionov <nowiwr01p@gmail.com>
2023-01-01 23:13:16 +0400

    Add buildSrc and manage dependencies

---
+++ buildSrc/src/main/java/com/nowiwr01p/buildsrc/common/libs.common.gradle.kts
@@ -0,0 +1,57 @@
+import com.nowiwr01p.buildsrc.dependency.BuildConfig
+import com.nowiwr01p.buildsrc.extensions.koinDependencies
+import com.nowiwr01p.buildsrc.extensions.accompanistDependencies
+import com.nowiwr01p.buildsrc.extensions.animationDependencies
+import com.nowiwr01p.buildsrc.extensions.biometricDependencies
+import com.nowiwr01p.buildsrc.extensions.roomDependencies
+import com.nowiwr01p.buildsrc.extensions.commonUiDependencies
+import com.nowiwr01p.buildsrc.extensions.navigationDependencies
+import com.nowiwr01p.buildsrc.extensions.unitTestDependencies
+import com.nowiwr01p.buildsrc.extensions.networkDependencies
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = BuildConfig.COMPILE_SDK
+    defaultConfig {
+        minSdk = BuildConfig.MIN_SDK
+        targetSdk = BuildConfig.TARGET_SDK
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    koinDependencies()
+    networkDependencies()
+    unitTestDependencies()
+}
---
+++ buildSrc/src/main/java/com/nowiwr01p/buildsrc/common/libs.ui.gradle.kts
@@ -0,0 +1,34 @@
+import com.nowiwr01p.buildsrc.dependency.BuildConfig
+import com.nowiwr01p.buildsrc.extensions.accompanistDependencies
+import com.nowiwr01p.buildsrc.extensions.koinDependencies
+import com.nowiwr01p.buildsrc.extensions.animationDependencies
+import com.nowiwr01p.buildsrc.extensions.biometricDependencies
+import com.nowiwr01p.buildsrc.extensions.roomDependencies
+import com.nowiwr01p.buildsrc.extensions.commonUiDependencies
+import com.nowiwr01p.buildsrc.extensions.navigationDependencies
+import com.nowiwr01p.buildsrc.extensions.unitTestDependencies
+import com.nowiwr01p.buildsrc.extensions.networkDependencies
+import org.gradle.kotlin.dsl.dependencies
+import com.nowiwr01p.buildsrc.dependency.Basic.Version
+
+plugins {
+    id("libs.common")
+}
+
+android{
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Version.COMPOSE
+    }
+}
+
+dependencies {
+    accompanistDependencies()
+    animationDependencies()
+    biometricDependencies()
+    roomDependencies()
+    commonUiDependencies()
+    navigationDependencies()
+}
