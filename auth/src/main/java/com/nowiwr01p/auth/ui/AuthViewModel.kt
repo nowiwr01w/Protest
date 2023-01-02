6eab008590e8d7eba280cf75beef2b28d936baf9
Andrey Larionov <nowiwr01p@gmail.com>
2023-01-02 22:20:10 +0400

    [Auth] UI

---
+++ auth/src/main/java/com/nowiwr01p/auth/ui/AuthViewModel.kt
@@ -0,0 +1,42 @@
+package com.nowiwr01p.auth.ui
+
+import com.nowiwr01p.auth.ui.AuthContract.*
+import com.nowiwr01p.auth.ui.data.AuthTextFieldType
+import com.nowiwr01p.auth.ui.data.AuthTextFieldType.*
+import com.nowiwr01p.auth.ui.data.AuthType.*
+import com.nowiwr01p.core_ui.view_model.BaseViewModel
+
+class AuthViewModel: BaseViewModel<Event, State, Effect>() {
+
+    override fun setInitialState() = State()
+
+    override fun handleEvents(event: Event) {
+        when (event) {
+            is Event.Init -> init()
+            is Event.OnAuthClick -> auth()
+            is Event.ToggleAuthMode -> toggleAuthMode()
+            is Event.OnValueChanged -> changeValue(event.type, event.value)
+        }
+    }
+
+    private fun init() {
+
+    }
+
+    private fun toggleAuthMode() = setState {
+        val authType = if (viewState.value.authType == SIGN_IN) SIGN_UP else SIGN_IN
+        copy(authType = authType)
+    }
+
+    private fun changeValue(type: AuthTextFieldType, value: String) = setState {
+        when (type) {
+            EMAIL -> copy(email = value)
+            PASSWORD -> copy(password = value)
+            PASSWORD_REPEAT -> copy(passwordRepeat = value)
+        }
+    }
+
+    private fun auth() {
+
+    }
+}
--- core-ui/src/main/java/com/nowiwr01p/core_ui/theme/Type.kt
+++ core-ui/src/main/java/com/nowiwr01p/core_ui/theme/Type.kt
@@ -4,25 +4,222 @@ import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
+    body1 = TextStyle( //lightTextPrimary
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
+        fontSize = 17.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    ),
+    h1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight(700),
+        fontSize = 27.sp,
+        lineHeight = 32.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.2.sp
+    ),
     button = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 17.sp,
+        lineHeight = 24.sp
+    )
+)
+
+val Typography.largeTitle
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Black,
+        fontSize = 36.sp,
+        lineHeight = 43.sp
+    )
+
+val Typography.title1Bold
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 27.sp,
+        lineHeight = 32.sp
+    )
+
+val Typography.title2
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp
+    )
+
+val Typography.title2Bold
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp
+    )
+
+val Typography.headline
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 17.sp,
+        lineHeight = 24.sp
+    )
+
+val Typography.subheadline
+    get() = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 15.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.2.sp
     )
-    */
-)
+
+val Typography.calloutMedium
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    )
+val Typography.calloutRegular
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    )
+val Typography.footnoteRegular
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 13.sp,
+        lineHeight = 16.sp
+    )
+
+val Typography.footnoteMedium
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 13.sp,
+        lineHeight = 18.sp
+    )
+
+val Typography.bodyRegular
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 17.sp,
+        lineHeight = 24.sp
+    )
+
+val Typography.subHeadlineRegular
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.2.sp
+    )
+
+val Typography.subHeadlineMedium
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 15.sp,
+        lineHeight = 20.sp
+    )
+
+val Typography.caption2Regular
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 13.sp,
+        lineHeight = 18.sp,
+        letterSpacing = 0.25.sp
+    )
+
+val Typography.caption3Medium
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 13.sp,
+        lineHeight = 18.sp
+    )
+
+val Typography.caption2Medium
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 14.sp
+    )
+
+val Typography.title2Medium
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 19.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    )
+
+val Typography.alertButton
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 1.25.sp
+    )
+
+val Typography.bottomNavigationItem
+    get() = TextStyle(
+        fontSize = 10.sp,
+        lineHeight = 12.sp,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.35.sp
+    )
+
+
+fun generateAutoSizeTextStyle(fontSize: TextUnit, lineHeight: TextUnit): TextStyle =
+    TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Black,
+        fontSize = fontSize,
+        lineHeight = lineHeight,
+        textAlign = TextAlign.Center
+    )
+
+val Typography.captionRegular
+    get() = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp
+    )
+
+val Typography.autoSizeStateBig
+    get() = generateAutoSizeTextStyle(fontSize = 48.sp, lineHeight = 56.sp)
+
+val Typography.autoSizeStateMiddle
+    get() = generateAutoSizeTextStyle(fontSize = 42.sp, lineHeight = 48.sp)
+
+val Typography.autoSizeStateSmall
+    get() = generateAutoSizeTextStyle(fontSize = 36.sp, lineHeight = 42.sp)
+
+val Typography.autoSizeStateExtraSmall
+    get() = generateAutoSizeTextStyle(fontSize = 24.sp, lineHeight = 28.sp)
