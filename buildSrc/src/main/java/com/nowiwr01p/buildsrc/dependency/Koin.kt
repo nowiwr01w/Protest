69d9b9444d23c107a3ec3c970df854b73d622efc
Andrey Larionov <nowiwr01w@pm.me>
2023-01-04 03:23:49 +0400

    Update dependencies: kotlin, accompanist + renaming

--- buildSrc/src/main/java/com/nowiwr01p/buildsrc/dependency/Koin.kt
+++ buildSrc/src/main/java/com/nowiwr01p/buildsrc/dependency/Koin.kt
@@ -4,8 +4,8 @@ import com.nowiwr01p.buildsrc.dependency.Koin.Version.KOIN
 
 object Koin {
 
-    const val KOIN_ANDROID = "io.insert-koin:koin-android:$KOIN"
-    const val KOIN_COMPOSE_LIB = "io.insert-koin:koin-androidx-compose:$KOIN"
+    const val ANDROID = "io.insert-koin:koin-android:$KOIN"
+    const val COMPOSE = "io.insert-koin:koin-androidx-compose:$KOIN"
 
     private object Version {
         const val KOIN = "3.2.2"
