6e4c1d7e5fe86d6f3f8df1882a0ebee8b0d1eb54
Andrey Larionov <nowiwr01@gmail.com>
2023-01-05 22:55:16 +0400

    [Maps] add maps dependencies and basic UI

---
+++ buildSrc/src/main/java/com/nowiwr01p/buildsrc/dependency/Map.kt
@@ -0,0 +1,19 @@
+package com.nowiwr01p.buildsrc.dependency
+
+import com.nowiwr01p.buildsrc.dependency.Map.Version.COMPOSE_FOUNDATION_VERSION
+import com.nowiwr01p.buildsrc.dependency.Map.Version.GOOGLE_MAP_VERSION
+import com.nowiwr01p.buildsrc.dependency.Map.Version.PLAY_SERVICES_VERSION
+
+object Map {
+
+    const val GOOGLE_MAP = "com.google.maps.android:maps-compose:$GOOGLE_MAP_VERSION"
+    const val PLAY_SERVICES = "com.google.android.gms:play-services-maps:$PLAY_SERVICES_VERSION"
+    const val COMPOSE_FOUNDATION = "androidx.compose.foundation:foundation:$COMPOSE_FOUNDATION_VERSION"
+    const val GOOGLE_MAP_WIDGETS = "com.google.maps.android:maps-compose-widgets:$GOOGLE_MAP_VERSION"
+
+    private object Version {
+        const val GOOGLE_MAP_VERSION = "2.8.0"
+        const val PLAY_SERVICES_VERSION = "18.0.2"
+        const val COMPOSE_FOUNDATION_VERSION = "1.4.0-alpha03"
+    }
+}
--- map/src/main/java/com/nowiwr01p/map/ui/MapMainScreen.kt
+++ map/src/main/java/com/nowiwr01p/map/ui/MapMainScreen.kt
@@ -1,15 +1,24 @@
 package com.nowiwr01p.map.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.nowiwr01p.core_ui.extensions.setSystemUiColor
 
 @Composable
-fun MapMainScreen() = Box(
-    modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Black)
-)
+fun MapMainScreen() {
+    setSystemUiColor()
+
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    )
+}
