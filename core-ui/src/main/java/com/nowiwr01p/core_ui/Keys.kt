72c57aa2fc3e97b6479af09115c8a8ee4edca1ac
Andrey Larionov <nowiwr01w@pm.me>
2023-01-06 00:27:44 +0400

    [Maps] add map with marker's click event

--- core-ui/src/main/java/com/nowiwr01p/core_ui/Keys.kt
+++ core-ui/src/main/java/com/nowiwr01p/core_ui/Keys.kt
@@ -2,4 +2,5 @@ package com.nowiwr01p.core_ui
 
 object Keys {
     const val ARG_TO_CITIES = "chosen_country"
+    const val ARG_CITY_LOCATION = "city_location"
 }
--- map/src/main/java/com/nowiwr01p/map/ui/MapMainScreen.kt
+++ map/src/main/java/com/nowiwr01p/map/ui/MapMainScreen.kt
@@ -2,23 +2,70 @@ package com.nowiwr01p.map.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
-import com.nowiwr01p.core_ui.extensions.setSystemUiColor
+import com.google.maps.android.compose.rememberMarkerState
+import com.nowiwr01p.core_ui.navigators.main.Navigator
+import com.nowiwr01p.core_ui.ui.CenterScreenProgressBar
+import com.nowiwr01p.core_ui.ui.EffectObserver
+import com.nowiwr01p.map.ui.MapContract.*
+import org.koin.androidx.compose.getViewModel
+import timber.log.Timber
 
 @Composable
-fun MapMainScreen() {
-    setSystemUiColor()
+fun MapMainScreen(
+    navigator: Navigator,
+    viewModel: MapViewModel = getViewModel()
+) {
+    val listener = object : Listener {
 
-    val singapore = LatLng(1.35, 103.87)
+    }
+
+    LaunchedEffect(Unit) {
+        viewModel.setEvent(Event.Init)
+    }
+
+    EffectObserver(viewModel.effect) {
+
+    }
+
+    MapMainScreenContent(viewModel.viewState.value, listener)
+}
+
+@Composable
+private fun MapMainScreenContent(
+    state: State,
+    listener: Listener?
+) {
+    if (state.showProgress) {
+        CenterScreenProgressBar()
+    } else {
+        MapContent(state, listener)
+    }
+}
+
+@Composable
+fun MapContent(
+    state: State,
+    listener: Listener?
+) {
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        position = CameraPosition.fromLatLngZoom(state.coordinates, 11f)
     }
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState
-    )
+    ) {
+        Marker(
+            state = rememberMarkerState(position = state.coordinates),
+            onClick = {
+                Timber.tag("Zhopa").d("click")
+                true
+            }
+        )
+    }
 }
