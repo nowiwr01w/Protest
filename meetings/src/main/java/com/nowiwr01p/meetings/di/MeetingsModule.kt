35373f96601e4c61315a54a922842e96532367e2
Andrey Larionov <nowiwr01w@pm.me>
2023-01-07 00:05:25 +0400

    [Meetings] Basic UI with empty list stub

--- meetings/src/main/java/com/nowiwr01p/meetings/di/MeetingsModule.kt
+++ meetings/src/main/java/com/nowiwr01p/meetings/di/MeetingsModule.kt
@@ -2,10 +2,14 @@ package com.nowiwr01p.meetings.di
 
 import com.nowiwr01p.core_ui.navigators.MeetingsNavigator
 import com.nowiwr01p.meetings.navigation.MeetingsNavigatorImpl
+import com.nowiwr01p.meetings.ui.MeetingsViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val moduleMeetings = module {
     single<MeetingsNavigator> {
         MeetingsNavigatorImpl()
     }
+
+    viewModel { MeetingsViewModel() }
 }
