dd2614b5406f2c2458e90ab93908f876dee7a12b
Andrey Larionov <nowiwr01w@pm.me>
2023-01-07 19:01:23 +0400

    [Meetings] Screen cache

---
+++ domain/src/main/java/com/nowiwr01p/domain/meetings/usecase/data/MeetingsScreenCache.kt
@@ -0,0 +1,5 @@
+package com.nowiwr01p.domain.meetings.usecase.data
+
+data class MeetingsScreenCache(
+    var data: MeetingsScreenCacheData = MeetingsScreenCacheData()
+)
--- meetings/src/main/java/com/nowiwr01p/meetings/di/MeetingsModule.kt
+++ meetings/src/main/java/com/nowiwr01p/meetings/di/MeetingsModule.kt
@@ -1,9 +1,13 @@
 package com.nowiwr01p.meetings.di
 
 import com.nowiwr01p.core_ui.navigators.MeetingsNavigator
+import com.nowiwr01p.domain.meetings.usecase.GetMeetingsScreenCacheUseCase
+import com.nowiwr01p.domain.meetings.usecase.SaveMeetingsScreenCacheUseCase
+import com.nowiwr01p.domain.meetings.usecase.data.MeetingsScreenCache
 import com.nowiwr01p.meetings.navigation.MeetingsNavigatorImpl
 import com.nowiwr01p.meetings.ui.MeetingsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val moduleMeetings = module {
@@ -11,5 +15,23 @@ val moduleMeetings = module {
         MeetingsNavigatorImpl()
     }
 
-    viewModel { MeetingsViewModel(get(), get()) }
+    viewModel {
+        val scope = getKoin().getOrCreateScope(meetingsScreenScopeId, named(meetingsScreenScopeName))
+
+        MeetingsViewModel(
+            get(),
+            get(),
+            scope.get(),
+            scope.get()
+        )
+    }
+
+    scope(named(meetingsScreenScopeName)) {
+        scoped { MeetingsScreenCache() }
+        scoped { GetMeetingsScreenCacheUseCase(get()) }
+        scoped { SaveMeetingsScreenCacheUseCase(get()) }
+    }
 }
+
+private const val meetingsScreenScopeId = "meetingsScreenScopeId"
+private const val meetingsScreenScopeName = "meetingsScreenScopeName"
