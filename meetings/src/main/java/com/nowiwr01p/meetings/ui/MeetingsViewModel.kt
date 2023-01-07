a9ce031722cdbdc9a3c84caa3b72314a7941e093
Andrey Larionov <nowiwr01@gmail.com>
2023-01-07 19:01:23 +0400

    [Meetings] Screen cache

--- meetings/src/main/java/com/nowiwr01p/meetings/ui/MeetingsViewModel.kt
+++ meetings/src/main/java/com/nowiwr01p/meetings/ui/MeetingsViewModel.kt
@@ -3,12 +3,15 @@ package com.nowiwr01p.meetings.ui
 import com.nowiwr01p.core_ui.view_model.BaseViewModel
 import com.nowiwr01p.domain.execute
 import com.nowiwr01p.domain.map.GetLocalUserUseCase
-import com.nowiwr01p.domain.meetings.usecase.GetCategoriesUseCase
+import com.nowiwr01p.domain.meetings.usecase.*
+import com.nowiwr01p.domain.meetings.usecase.data.MeetingsScreenCacheData
 import com.nowiwr01p.meetings.ui.MeetingsContract.*
 
 class MeetingsViewModel(
     private val getCategories: GetCategoriesUseCase,
-    private val getLocalUserUseCase: GetLocalUserUseCase
+    private val getLocalUserUseCase: GetLocalUserUseCase,
+    private val getMeetingsScreenCacheUseCase: GetMeetingsScreenCacheUseCase,
+    private val saveMeetingsScreenCacheUseCase: SaveMeetingsScreenCacheUseCase
 ): BaseViewModel<Event, State, Effect>() {
 
     override fun setInitialState() = State()
@@ -22,20 +25,51 @@ class MeetingsViewModel(
     private fun init() = io {
         setState { copy(showProgress = true) }
         runCatching {
+            getScreenCache()
             getUserData()
             getCategories()
         }.onSuccess {
+            saveScreenCache()
             setState { copy(showProgress = false) }
         }
     }
 
+    /**
+     * LOCAL USER DATA
+     */
     private suspend fun getUserData() {
         val user = getLocalUserUseCase.execute()
         setState { copy(user = user) }
     }
 
+    /**
+     * CATEGORIES
+     */
     private suspend fun getCategories() {
         val categories = getCategories.execute()
         setState { copy(categories = categories) }
+    }
+
+    /**
+     * SCREEN CACHE
+     */
+    private suspend fun getScreenCache() = getMeetingsScreenCacheUseCase.execute()?.let { cache ->
+        setState {
+            copy(
+                user = cache.data.user,
+                meetings = cache.data.meetings,
+                categories = cache.data.categories,
+                showProgress = false
+            )
+        }
+    }
+
+    private suspend fun saveScreenCache() = with(viewState.value) {
+        val data = MeetingsScreenCacheData(
+            user = user,
+            meetings = meetings,
+            categories = categories
+        )
+        saveMeetingsScreenCacheUseCase.execute(data)
     }
 }
