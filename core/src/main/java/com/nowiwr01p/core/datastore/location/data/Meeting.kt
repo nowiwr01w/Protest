09d49ed789292346995b2789685cf342b0bbe191
Andrey Larionov <nowiwr01w@pm.me>
2023-01-08 19:44:25 +0400

    [Meeting Screen] Change Meeting.kt item

--- core/src/main/java/com/nowiwr01p/core/datastore/location/data/Meeting.kt
+++ core/src/main/java/com/nowiwr01p/core/datastore/location/data/Meeting.kt
@@ -1,33 +1,41 @@
 package com.nowiwr01p.core.datastore.location.data
 
+import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@Serializable
 data class Meeting(
-    @SerialName("id")
+    @SerializedName("id")
     val id: String = "",
-    @SerialName("title")
+    @SerializedName("creatorId")
+    val creatorId: String = "",
+    @SerializedName("image")
+    val image: String = "",
+    @SerializedName("date")
+    val date: Long = 0L,
+    @SerializedName("openDate")
+    val openDate: OpenDate = OpenDate(),
+    @SerializedName("categories")
+    val categories: List<String> = listOf(),
+    @SerializedName("title")
     val title: String = "",
-    @SerialName("description")
+    @SerializedName("description")
     val description: String = "",
-    @SerialName("telegram")
+    @SerializedName("locationInfo")
+    val locationInfo: LocationInfo = LocationInfo(),
+    @SerializedName("hideLocationUntil")
+    val hideLocationUntil: HideLocationUntil = HideLocationUntil(),
+    @SerializedName("takeWithYouInfo")
+    val takeWithYouInfo: TakeWithYouInfo = TakeWithYouInfo(),
+    @SerializedName("details")
+    val details: Details = Details(),
+    @SerializedName("reaction")
+    val reaction: Reaction = Reaction(),
+    @SerializedName("telegram")
     val telegram: String = "",
-    @SerialName("path")
+    @SerializedName("path")
     val path: List<Coordinate> = listOf(),
-    @SerialName("startDate")
-    val startDate: Long = 0L,
-    @SerialName("endDate")
-    val endDate: Long = 0L,
-    @SerialName("people")
-    val people: Int = 1,
-    @SerialName("leaders")
-    val leaders: Int = 1,
-    @SerialName("everyday")
-    val everyday: Boolean = false,
-    @SerialName("secret")
-    val secret: Boolean = false,
-    @SerialName("currentPosition")
+    @SerializedName("currentPosition")
     val currentPosition: Coordinate = Coordinate()
 )
 
@@ -37,4 +45,58 @@ data class Coordinate(
     val latitude: Double = .0,
     @SerialName("longitude")
     val longitude: Double = .0,
+)
+
+@Serializable
+data class OpenDate(
+    @SerializedName("test")
+    val test: String = "",
+    @SerializedName("requiredPeopleCount")
+    val requiredPeopleCount: Int = 0
+)
+
+@Serializable
+data class HideLocationUntil(
+    @SerializedName("date")
+    val date: Long = 0L,
+    @SerializedName("requiredPeopleCount")
+    val requiredPeopleCount: Int = 0
+)
+
+@Serializable
+data class LocationInfo(
+    @SerializedName("shortName")
+    val shortName: String = "",
+    @SerializedName("coordinates")
+    val coordinates: Coordinate = Coordinate(),
+    @SerializedName("placeDetails")
+    val placeDetails: String = ""
+)
+
+@Serializable
+data class TakeWithYouInfo(
+    @SerializedName("description")
+    val description: String = "",
+    @SerializedName("posterLinks")
+    val posterLinks: List<String> = listOf()
+)
+
+@Serializable
+data class Details(
+    @SerializedName("goals")
+    val goals: List<String> = listOf(),
+    @SerializedName("slogans")
+    val slogans: List<String> = listOf(),
+    @SerializedName("strategy")
+    val strategy: List<String> = listOf(),
+)
+
+@Serializable
+data class Reaction(
+    @SerializedName("peopleGoCount")
+    val peopleGoCount: Int = 0,
+    @SerializedName("peopleMaybeGoCount")
+    val peopleMaybeGoCount: Int = 0,
+    @SerializedName("yourChoiceToGo")
+    val yourChoiceToGo: Boolean? = null
 )
