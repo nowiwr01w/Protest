b712c093e3d791ac5945f8cecdb34018c7d9fda1
Andrey Larionov <nowiwr01w@pm.me>
2023-01-10 19:01:03 +0400

    [Meeting Screen] Reaction buttons UI

--- meetings/src/main/java/com/nowiwr01p/meetings/ui/meeting/MeetingContract.kt
+++ meetings/src/main/java/com/nowiwr01p/meetings/ui/meeting/MeetingContract.kt
@@ -1,10 +1,11 @@
 package com.nowiwr01p.meetings.ui.meeting
 
-import com.nowiwr01p.core.datastore.location.data.City
 import com.nowiwr01p.core.datastore.location.data.Meeting
+import com.nowiwr01p.core.model.User
 import com.nowiwr01p.core_ui.view_model.ViewEvent
 import com.nowiwr01p.core_ui.view_model.ViewSideEffect
 import com.nowiwr01p.core_ui.view_model.ViewState
+import com.nowiwr01p.meetings.ui.meeting.MeetingContract.ReactionLoadingStatus.*
 
 interface MeetingContract {
 
@@ -14,9 +15,15 @@ interface MeetingContract {
     }
 
     data class State(
-        val city: City = City(),
-        val meeting: Meeting = Meeting.getSampleData()
+        val user: User = User(),
+        val meeting: Meeting = Meeting.getSampleData(),
+        val positiveReaction: ReactionLoadingStatus = DEFAULT,
+        val negativeReaction: ReactionLoadingStatus = DEFAULT,
     ): ViewState
+
+    enum class ReactionLoadingStatus {
+        DEFAULT, SEND_REQUEST, SUCCESS, ERROR
+    }
 
     sealed interface Effect: ViewSideEffect {
 
