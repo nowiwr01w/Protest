8361f3278ae286bff7b45d07f8a3792ad39acd9e
wagnod <simvarya1404@gmail.com>
2023-01-10 20:57:44 +0400

    [News] UI - add Article Screen

--- news/src/main/java/com/nowiwr01p/news/navigation/NewsScreenType.kt
+++ news/src/main/java/com/nowiwr01p/news/navigation/NewsScreenType.kt
@@ -1,5 +1,8 @@
 package com.nowiwr01p.news.navigation
 
+import com.nowiwr01p.core_ui.Keys
+
 enum class NewsScreenType(val route: String) {
-    NewsMainScreen("news_main_screen")
+    NewsMainScreen("news_main_screen"),
+    NewsArticleScreen("news_article_screen?chosen_article={${Keys.ARG_ARTICLE}}")
 }
