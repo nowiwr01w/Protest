78810b1369a340725731edc0b1a9f3bdad975421
Andrey Larionov <nowiwr01w@pm.me>
2023-01-10 14:56:08 +0400

    [Meetings] Hidden map feature

--- meetings/src/main/java/com/nowiwr01p/meetings/ui/meeting/MeetingViewModel.kt
+++ meetings/src/main/java/com/nowiwr01p/meetings/ui/meeting/MeetingViewModel.kt
@@ -2,9 +2,13 @@ package com.nowiwr01p.meetings.ui.meeting
 
 import com.nowiwr01p.core.datastore.location.data.Meeting
 import com.nowiwr01p.core_ui.view_model.BaseViewModel
+import com.nowiwr01p.domain.execute
+import com.nowiwr01p.domain.location.usecase.local.GetLocalCityUseCase
 import com.nowiwr01p.meetings.ui.meeting.MeetingContract.*
 
-class MeetingViewModel: BaseViewModel<Event, State, Effect>() {
+class MeetingViewModel(
+    private val getLocalCityUseCase: GetLocalCityUseCase
+): BaseViewModel<Event, State, Effect>() {
 
     override fun setInitialState() = State()
 
@@ -14,7 +18,16 @@ class MeetingViewModel: BaseViewModel<Event, State, Effect>() {
         }
     }
 
-    private fun init(meeting: Meeting) {
-        setState { copy(meeting = meeting) }
+    private fun init(meeting: Meeting) = io {
+        runCatching {
+            getUserCity()
+        }.onSuccess {
+            setState { copy(meeting = meeting) }
+        }
+    }
+
+    private suspend fun getUserCity() {
+        val city = getLocalCityUseCase.execute()
+        setState { copy(city = city) }
     }
 }
