b739d692d5d616bf90035864cbba9dd1a7dc7852
Andrey Larionov <nowiwr01w@pm.me>
2023-01-13 18:54:09 +0400

    [Create Meeting] Change OpenDate logic

--- meetings/src/main/java/com/nowiwr01p/meetings/ui/create_meeting/CreateMeetingContract.kt
+++ meetings/src/main/java/com/nowiwr01p/meetings/ui/create_meeting/CreateMeetingContract.kt
@@ -5,18 +5,19 @@ import com.nowiwr01p.core.model.Category
 import com.nowiwr01p.core_ui.view_model.ViewEvent
 import com.nowiwr01p.core_ui.view_model.ViewSideEffect
 import com.nowiwr01p.core_ui.view_model.ViewState
-import com.nowiwr01p.meetings.ui.create_meeting.data.CheckBoxType
 import com.nowiwr01p.meetings.ui.create_meeting.data.DetailsItemType
 
 interface CreateMeetingContract {
 
     sealed interface Event: ViewEvent {
         object Init: Event
+        object ShowDateTimePicker: Event
+        object NavigateToMapDrawPath: Event
+        object NavigateToChooseStartLocation: Event
         data class ShowCategoriesBottomSheet(val content: @Composable () -> Unit): Event
         data class OnSelectedCategoryClick(val category: Category): Event
         data class OnAddDetailsItemClick(val type: DetailsItemType): Event
         data class OnRemoveDetailsItemClick(val type: DetailsItemType, val index: Int): Event
-        data class SetCheckBoxState(val type: CheckBoxType, val value: Boolean): Event
     }
 
     data class State(
@@ -25,18 +26,21 @@ interface CreateMeetingContract {
         val slogans: List<String> = listOf(),
         val strategy: List<String> = listOf(),
         val categories: List<Category> = listOf(),
-        val selectedCategories: Set<Category> = setOf(),
-        val isDateCheckBoxChecked: Boolean = false,
-        val isOpenDateCheckBoxChecked: Boolean = false
+        val selectedCategories: Set<Category> = setOf()
     ): ViewState
 
-    sealed interface Effect: ViewSideEffect
+    sealed interface Effect: ViewSideEffect {
+        object NavigateToMapDrawPath: Effect
+        object NavigateToChooseStartLocation: Effect
+    }
 
     interface Listener {
         fun onBackClick()
-        fun setCheckBoxState(type: CheckBoxType, value: Boolean)
         fun onAddDetailsItem(type: DetailsItemType)
         fun onRemoveDetailsType(type: DetailsItemType, index: Int)
         fun showCategoriesBottomSheet()
+        fun showDateTimePicker()
+        fun navigateToMapDrawPath()
+        fun navigateChooseStartLocation()
     }
 }
