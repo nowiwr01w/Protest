59ebd49756ddd6ce0ef62483ed5468d5f43bfe75
Andrey Larionov <nowiwr01w@pm.me>
2023-01-13 15:01:27 +0400

    [Create Meeting] Categories Bottom Sheet

--- meetings/src/main/java/com/nowiwr01p/meetings/ui/create_meeting/CreateMeetingVewModel.kt
+++ meetings/src/main/java/com/nowiwr01p/meetings/ui/create_meeting/CreateMeetingVewModel.kt
@@ -1,5 +1,7 @@
 package com.nowiwr01p.meetings.ui.create_meeting
 
+import com.nowiwr01p.core.model.Category
+import com.nowiwr01p.core_ui.ui.bottom_sheet.ShowBottomSheetHelper
 import com.nowiwr01p.core_ui.view_model.BaseViewModel
 import com.nowiwr01p.domain.cteate_meeting.GetCachedCategoriesUseCase
 import com.nowiwr01p.domain.execute
@@ -11,7 +13,8 @@ import com.nowiwr01p.meetings.ui.create_meeting.data.DetailsItemType
 import com.nowiwr01p.meetings.ui.create_meeting.data.DetailsItemType.*
 
 class CreateMeetingVewModel(
-    private val getCachedCategoriesUseCase: GetCachedCategoriesUseCase
+    private val getCachedCategoriesUseCase: GetCachedCategoriesUseCase,
+    private val showBottomSheetHelper: ShowBottomSheetHelper
 ): BaseViewModel<Event, State, Effect>() {
 
     override fun setInitialState() = State()
@@ -22,6 +25,8 @@ class CreateMeetingVewModel(
             is Event.SetCheckBoxState -> setCheckBoxState(event.type, event.value)
             is Event.OnAddDetailsItemClick -> changeDetailsListState(event.type)
             is Event.OnRemoveDetailsItemClick -> changeDetailsListState(event.type, event.index)
+            is Event.OnSelectedCategoryClick -> selectCategory(event.category)
+            is Event.ShowCategoriesBottomSheet -> showBottomSheetHelper.showBottomSheet(event.content)
         }
     }
 
@@ -37,6 +42,16 @@ class CreateMeetingVewModel(
     private suspend fun getCategories() {
         val categories = getCachedCategoriesUseCase.execute()
         setState { copy(categories = categories) }
+    }
+
+    /**
+     * SELECTED CATEGORIES
+     */
+    private fun selectCategory(category: Category) = with(viewState.value) {
+        val updated = selectedCategories.toMutableSet().apply {
+            if (category.isSelected) remove(category) else add(category)
+        }
+        setState { copy(selectedCategories = updated) }
     }
 
     /**
