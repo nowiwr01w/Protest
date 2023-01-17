855f7ae7855380cb659ed370b594b3997ba7f38a
Andrey Larionov <nowiwr01w@pm.me>
2023-01-17 23:36:11 +0400

    [Meetings] Add meeting ID

--- core/src/main/java/com/nowiwr01p/core/datastore/cities/data/Meeting.kt
+++ core/src/main/java/com/nowiwr01p/core/datastore/cities/data/Meeting.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Meeting(
+    val id: String = "",
     val cityName: String = "",
     val creatorId: String = "",
     val image: String = "",
