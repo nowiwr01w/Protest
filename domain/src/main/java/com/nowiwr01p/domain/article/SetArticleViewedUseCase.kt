fc8b8b8fa178d546bca6bce90022ff9691cccbcc
Andrey Larionov <nowiwr01w@pm.me>
2023-01-17 23:33:00 +0400

    [Article] Article views

---
+++ domain/src/main/java/com/nowiwr01p/domain/article/SetArticleViewedUseCase.kt
@@ -0,0 +1,12 @@
+package com.nowiwr01p.domain.article
+
+import com.nowiwr01p.domain.UseCase
+
+class SetArticleViewedUseCase(
+    private val repository: ArticleRepository
+): UseCase<String, Int> {
+
+    override suspend fun execute(input: String): Int {
+        return repository.setArticleViewed(input)
+    }
+}
