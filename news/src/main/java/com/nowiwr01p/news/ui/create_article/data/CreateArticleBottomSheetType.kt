6db1914640a269b79f9afeb42ec6c84b21aeaf76
Andrey Larionov <nowiwr01@gmail.com>
2023-01-18 21:00:44 +0400

    [Create Article] Change static fields - logic

---
+++ news/src/main/java/com/nowiwr01p/news/ui/create_article/data/CreateArticleBottomSheetType.kt
@@ -0,0 +1,8 @@
+package com.nowiwr01p.news.ui.create_article.data
+
+enum class CreateArticleBottomSheetType(val type: String) {
+    SUBTITLE("Подзаголовок"),
+    TEXT("Текст"),
+    IMAGE("Картинка"),
+    ORDERED_LIST("Упорядоченный список")
+}
