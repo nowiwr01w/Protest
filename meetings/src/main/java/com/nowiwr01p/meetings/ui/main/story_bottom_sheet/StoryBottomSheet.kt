4da5115b6789bc8c38ca45d1b09626d0fb1629a5
Andrey Larionov <nowiwr01w@pm.me>
2023-01-23 22:56:04 +0400

    [Meetings] View stories - add images

--- meetings/src/main/java/com/nowiwr01p/meetings/ui/main/story_bottom_sheet/StoryBottomSheet.kt
+++ meetings/src/main/java/com/nowiwr01p/meetings/ui/main/story_bottom_sheet/StoryBottomSheet.kt
@@ -3,18 +3,18 @@ package com.nowiwr01p.meetings.ui.main.story_bottom_sheet
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.nowiwr01p.core_ui.theme.textPrimary
-import com.nowiwr01p.core_ui.theme.title2Bold
-import com.nowiwr01p.core_ui.theme.title3Bold
-import com.nowiwr01p.domain.meetings.data.Description
-import com.nowiwr01p.domain.meetings.data.OrderedList
-import com.nowiwr01p.domain.meetings.data.Story
-import com.nowiwr01p.domain.meetings.data.Title
+import com.nowiwr01p.core_ui.theme.*
+import com.nowiwr01p.domain.meetings.data.*
+import com.skydoves.landscapist.coil.CoilImage
 
 /**
  * STORY BOTTOM SHEET
@@ -23,8 +23,10 @@ internal fun StoryBottomSheet(story: Story): @Composable () -> Unit = {
     LazyColumn(
         modifier = Modifier.fillMaxSize()
     ) {
+        item { Spacer(modifier = Modifier.height(8.dp)) }
         items(story.getContent()) { item ->
             when (item) {
+                is Image -> Image(item)
                 is Title -> Title(item.text)
                 is Description -> Description(item.text)
                 is OrderedList -> OrderedListItem(item)
@@ -39,7 +41,7 @@ private fun Title(text: String) = Text(
     text = text,
     color = MaterialTheme.colors.textPrimary,
     style = MaterialTheme.typography.title2Bold,
-    modifier = Modifier.padding(top = 16.dp, start = 16.dp)
+    modifier = Modifier.padding(top = 8.dp, start = 16.dp)
 )
 
 @Composable
@@ -49,6 +51,30 @@ private fun Description(text: String) = Text(
     style = MaterialTheme.typography.body1,
     modifier = Modifier.padding(top = 8.dp, start = 16.dp, end = 16.dp)
 )
+
+@Composable
+private fun Image(image: Image) = Column(
+    horizontalAlignment = Alignment.CenterHorizontally,
+    modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 12.dp, bottom = 4.dp),
+) {
+    CoilImage(
+        imageModel = { image.link },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(250.dp)
+    )
+    Text(
+        text = image.description,
+        style = MaterialTheme.typography.footnoteRegular,
+        color = MaterialTheme.colors.textColorSecondary,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 4.dp, start = 24.dp, end = 24.dp)
+    )
+}
 
 @Composable
 private fun OrderedListItem(orderedList: OrderedList) = Column(
