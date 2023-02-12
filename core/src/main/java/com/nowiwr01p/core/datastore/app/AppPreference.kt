9753e0f0f9d5e7f66bcebf293d5efa2d00d731b0
nowiwr01w <nowiwr01w@pm.me>
2023-02-12 17:26:11 +0400

    Show putin huylo animation by default

--- core/src/main/java/com/nowiwr01p/core/datastore/app/AppPreference.kt
+++ core/src/main/java/com/nowiwr01p/core/datastore/app/AppPreference.kt
@@ -4,5 +4,5 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class AppPreference(
-    val splashScreenDemoAnimation: Boolean = true
+    val splashScreenDemoAnimation: Boolean = false
 )
